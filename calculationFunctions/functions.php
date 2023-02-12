5457901f47f39a8318d43b5c366552d1d471acf6
habib24457 <habiburehman390@gmail.com>
2023-02-12 12:09:08 +0100

    Separated functions

--- calculationFunctions/functions.php
+++ calculationFunctions/functions.php
@@ -1,18 +1,37 @@
 <?php
-function sum ($firstNumber, $secondNumber){
+
+#add two numbers
+function sum($firstNumber, $secondNumber)
+{
     $total = $firstNumber + $secondNumber;
 
     return $total;
 }
 
-$callSum = sum(5,5);
-
-echo 'Total number is = '.$callSum;
-
-function isEven($randomNumber){
-    if($randomNumber %2 == 0){
+#check if number is even or odd
+function isEven($randomNumber)
+{
+    if ($randomNumber % 2 == 0) {
         return true;
-    }else {
+    } else {
         return false;
     }
+}
+
+#strict type
+function factorial(int $number)
+{
+    $result = 1;
+
+    for ($i = $number; $i > 1; $i--) {
+        $result *= $i;
+    }
+    return $result;
+}
+
+
+#function that returns a random number on each call
+function randomNumberGenerator(int $min, int $max)
+{
+    return mt_rand($min, $max);
 }
--- myFunc.php
+++ myFunc.php
@@ -1,9 +1,19 @@
 <?php
 include_once "./calculationFunctions/functions.php";
 
+$res =  sum(3, 3);
 
+echo 'Total = ' . $res;
 
+$num = 3;
 
+$factorialOfNum = factorial($num);
 
+echo 'Factorial of ' . $num . ' is =' . $factorialOfNum;
 
-?>
+$max = 3;
+$min = 1;
+
+$ran = randomNumberGenerator($min, $max);
+
+echo '\n Random Number between ' . $max . 'and ' . $min . '=' . $ran;
